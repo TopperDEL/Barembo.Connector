7c593636c9b70defd995f32bdbf7108644ecb74e
TopperDEL <github_com@tparth.de>
2020-10-06 20:43:17 +0200

    fix: Add IEntryService to git

---
+++ Barembo.Connector/Interfaces/IEntryService.cs
@@ -0,0 +1,29 @@
+﻿using Barembo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barembo.Interfaces
+{
+    /// <summary>
+    /// An EntryService is responsible for loading and saving an Entry
+    /// </summary>
+    public interface IEntryService
+    {
+        /// <summary>
+        /// Loads an Entry.
+        /// </summary>
+        /// <param name="entryRef">The EntryReference of an Entry to load</param>
+        /// <returns>The Entry if it exists, otherwise throws a EntryNotExistsException</returns>
+        Task<Entry> LoadAsync(EntryReference entryRef);
+
+        /// <summary>
+        /// Saves an Entry using.
+        /// </summary>
+        /// <param name="entryRef">The EntryReference of an Entry to save</param>
+        /// <param name="entryToSave">The Entry to save</param>
+        /// <returns>true, if the Book could be saved</returns>
+        Task<bool> SaveAsync(EntryReference entryRef, Entry entryToSave);
+    }
+}
