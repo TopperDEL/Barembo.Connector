40034500d407d6d457fbaa0f185f666b9a2f6902
TopperDEL <github_com@tparth.de>
2020-10-07 21:31:03 +0200

    fix: Correct docs

--- Barembo.Connector/Interfaces/IEntryService.cs
+++ Barembo.Connector/Interfaces/IEntryService.cs
@@ -19,11 +19,11 @@ namespace Barembo.Interfaces
         Task<Entry> LoadAsync(EntryReference entryRef);
 
         /// <summary>
-        /// Saves an Entry using.
+        /// Saves an Entry using an EntryReference.
         /// </summary>
         /// <param name="entryRef">The EntryReference of an Entry to save</param>
         /// <param name="entryToSave">The Entry to save</param>
-        /// <returns>true, if the Book could be saved</returns>
+        /// <returns>true, if the Entry could be saved</returns>
         Task<bool> SaveAsync(EntryReference entryRef, Entry entryToSave);
 
         /// <summary>
