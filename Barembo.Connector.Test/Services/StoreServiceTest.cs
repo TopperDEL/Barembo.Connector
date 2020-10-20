b05e0df99dff7aaff2207e413ce31be5a959d426
TopperDEL <github_com@tparth.de>
2020-10-20 21:46:06 +0200

    feat: StoreService

---
+++ Barembo.Connector.Test/Services/StoreServiceTest.cs
@@ -0,0 +1,93 @@
+﻿using Barembo.Models;
+using Barembo.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Barembo.Connector.Test.Services
+{
+    [TestClass]
+    public partial class StoreServiceTest
+    {
+        /// <summary>
+        /// Create another CS-file "StoreServiceTestSecret.cs" and add the following:
+        /// public partial class StoreServiceTest
+        /// {
+        ///     static StoreServiceTest()
+        ///     {
+        ///         _accessGrantForTesting = ""; //Add an AccessGrant for your tests
+        ///     }
+        /// }
+        /// 
+        /// Be sure to exclude this file from Git to now expose your Access!
+        /// </summary>
+        static string _accessGrantForTesting;
+
+        StoreService _storeService;
+        StoreAccess _storeAccess;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _storeService = new StoreService("barembo-test");
+            _storeAccess = new StoreAccess(_accessGrantForTesting);
+        }
+
+        [TestMethod]
+        public async Task PutAsJson_Uploads_AsJson()
+        {
+            Book book = new Book();
+            book.Name = "Test-Book - " + DateTime.Now.ToString();
+
+            var result = await _storeService.PutObjectAsJsonAsync<Book>(_storeAccess, StoreKey.Book(book.Id), book);
+
+            Assert.IsTrue(result);
+
+            var downloaded = await _storeService.GetObjectFromJsonAsync<Book>(_storeAccess, StoreKey.Book(book.Id));
+
+            Assert.AreEqual(book.Id, downloaded.Id);
+            Assert.AreEqual(book.Name, downloaded.Name);
+        }
+
+        [TestMethod]
+        public async Task List_Finds_Objects()
+        {
+            Book book = new Book();
+            book.Name = "Test-Book - " + DateTime.Now.ToString();
+            Entry entry1 = new Entry();
+            Entry entry2 = new Entry();
+
+            await _storeService.PutObjectAsJsonAsync<Entry>(_storeAccess, StoreKey.Entry(book.Id, "entry1","contrib1"), entry1);
+            await _storeService.PutObjectAsJsonAsync<Entry>(_storeAccess, StoreKey.Entry(book.Id, "entry2", "contrib2"), entry2);
+
+            var list = await _storeService.ListObjectsAsync(_storeAccess, StoreKey.Entries(book.Id));
+
+            Assert.AreEqual(2, list.Count());
+        }
+
+        [TestMethod]
+        public async Task PutAsStream_Uploads_AsStream()
+        {
+            string stringToStream = "This is a stream test";
+            string testGuid = Guid.NewGuid().ToString();
+            MemoryStream mstream = new MemoryStream(Encoding.UTF8.GetBytes(stringToStream));
+
+            var result = await _storeService.PutObjectFromStreamAsync(_storeAccess, StoreKey.Book(testGuid), mstream);
+
+            Assert.IsTrue(result);
+
+            var downloaded = await _storeService.GetObjectAsStreamAsync(_storeAccess, StoreKey.Book(testGuid));
+            byte[] downloadedBytes = new byte[mstream.Length];
+            downloaded.Read(downloadedBytes, 0, (int)mstream.Length);
+            MemoryStream downloadStream = new MemoryStream(downloadedBytes);
+
+            var downloadedString = Encoding.UTF8.GetString(downloadStream.ToArray());
+
+            Assert.AreEqual(stringToStream, downloadedString);
+        }
+    }
+}
---
+++ Barembo.Connector/Services/StoreService.cs
@@ -0,0 +1,159 @@
+﻿using Barembo.Interfaces;
+using Barembo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using uplink.NET.Interfaces;
+using uplink.NET.Models;
+using uplink.NET.Services;
+using System.Linq;
+
+namespace Barembo.Services
+{
+    public class StoreService : IStoreService
+    {
+        private static Dictionary<string, IBucketService> _bucketServiceInstances = new Dictionary<string, IBucketService>();
+        private static Dictionary<string, IObjectService> _objectServiceInstances = new Dictionary<string, IObjectService>();
+        private static Dictionary<string, Bucket> _BucketInstances = new Dictionary<string, Bucket>();
+
+        private static string _bucketName;
+
+        public StoreService(string bucketName = "barembo")
+        {
+            _bucketName = bucketName;
+        }
+
+        public async Task<Stream> GetObjectAsStreamAsync(StoreAccess access, StoreKey storeKey)
+        {
+            var bucket = await GetBucketAsync(access);
+
+            var objectInfo = await GetObjectInfoAsync(access, storeKey);
+            if (objectInfo.ObjectExists)
+            {
+                return new DownloadStream(bucket, (int)objectInfo.Size, storeKey.ToString());
+            }
+            else
+                return null;
+        }
+
+        public async Task<T> GetObjectFromJsonAsync<T>(StoreAccess access, StoreKey storeKey)
+        {
+            var objectService = GetObjectService(access);
+            var bucket = await GetBucketAsync(access);
+
+            var download = await objectService.DownloadObjectAsync(bucket, storeKey.ToString(), new DownloadOptions(), false);
+            await download.StartDownloadAsync();
+
+            if (download.Completed)
+            {
+                return DeserializeFromJSON<T>(download.DownloadedBytes);
+            }
+            else
+                return default(T);
+        }
+
+        public async Task<StoreObjectInfo> GetObjectInfoAsync(StoreAccess access, StoreKey storeKey)
+        {
+            var objectService = GetObjectService(access);
+            var bucket = await GetBucketAsync(access);
+
+            try
+            {
+                var objectInfo = await objectService.GetObjectAsync(bucket, storeKey.ToString());
+                return new StoreObjectInfo() { ObjectExists = true, Size = objectInfo.SystemMetaData.ContentLength };
+            }
+            catch(Exception)
+            {
+                return new StoreObjectInfo() { ObjectExists = false };
+            }
+        }
+
+        public async Task<IEnumerable<StoreObject>> ListObjectsAsync(StoreAccess access, StoreKey storeKey)
+        {
+            var objectService = GetObjectService(access);
+            var bucket = await GetBucketAsync(access);
+
+            var listObjectsOption = new ListObjectsOptions();
+            listObjectsOption.Prefix = storeKey.ToString();
+            listObjectsOption.Recursive = true;
+
+            var objects = await objectService.ListObjectsAsync(bucket, listObjectsOption);
+
+            return objects.Items.Select(i => new StoreObject(i.Key, i.Key.Replace(listObjectsOption.Prefix, "")));
+        }
+
+        public async Task<bool> PutObjectAsJsonAsync<T>(StoreAccess access, StoreKey storeKey, T objectToPut)
+        {
+            var objectService = GetObjectService(access);
+            var bucket = await GetBucketAsync(access);
+
+            var JSONBytes = SerializeToJSON(objectToPut);
+
+            var upload = await objectService.UploadObjectAsync(bucket, storeKey.ToString(), new UploadOptions(), JSONBytes, false);
+            await upload.StartUploadAsync();
+
+            return upload.Completed;
+        }
+
+        public async Task<bool> PutObjectFromStreamAsync(StoreAccess access, StoreKey storeKey, Stream objectToPut)
+        {
+            var objectService = GetObjectService(access);
+            var bucket = await GetBucketAsync(access);
+
+            var upload = await objectService.UploadObjectAsync(bucket, storeKey.ToString(), new UploadOptions(), objectToPut, false);
+            await upload.StartUploadAsync(); //ToDo: Place Stream in UploadQueue
+
+            return upload.Completed;
+        }
+
+        private static IBucketService GetBucketService(StoreAccess access)
+        {
+            if (_bucketServiceInstances.ContainsKey(access.AccessGrant))
+                return _bucketServiceInstances[access.AccessGrant];
+
+            Access storjAccess = new Access(access.AccessGrant);
+            var bucketService = new BucketService(storjAccess);
+            _bucketServiceInstances.Add(access.AccessGrant, bucketService);
+
+            return bucketService;
+        }
+
+        private static IObjectService GetObjectService(StoreAccess access)
+        {
+            if (_objectServiceInstances.ContainsKey(access.AccessGrant))
+                return _objectServiceInstances[access.AccessGrant];
+
+            Access storjAccess = new Access(access.AccessGrant);
+            var objectService = new ObjectService(storjAccess);
+            _objectServiceInstances.Add(access.AccessGrant, objectService);
+
+            return objectService;
+        }
+
+        private static async Task<Bucket> GetBucketAsync(StoreAccess access)
+        {
+            if (_BucketInstances.ContainsKey(access.AccessGrant))
+                return _BucketInstances[access.AccessGrant];
+
+            var bucketService = GetBucketService(access);
+            var bucket = await bucketService.EnsureBucketAsync(_bucketName);
+            _BucketInstances.Add(access.AccessGrant, bucket);
+
+            return bucket;
+        }
+
+        private static byte[] SerializeToJSON(object objectToSerialize)
+        {
+            var JSONString = Newtonsoft.Json.JsonConvert.SerializeObject(objectToSerialize);
+            return Encoding.UTF8.GetBytes(JSONString);
+        }
+
+        private static T DeserializeFromJSON<T>(byte[] JSONBytes)
+        {
+            var JSONString = Encoding.UTF8.GetString(JSONBytes);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(JSONString);
+        }
+    }
+}
