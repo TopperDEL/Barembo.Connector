6db019de29e4e05e1c11c8692f0815fbdcd1d47d
TopperDEL <github_com@tparth.de>
2021-01-03 11:39:54 +0100

    feat: Switch over to ImageSharp for Thumbnails

--- Barembo.Connector/Services/ThumbnailGeneratorService.cs
+++ Barembo.Connector/Services/ThumbnailGeneratorService.cs
@@ -1,7 +1,9 @@
 ﻿using Barembo.Interfaces;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +12,17 @@ namespace Barembo.Services
 {
     public class ThumbnailGeneratorService : IThumbnailGeneratorService
     {
-        public bool ThumbnailCallback()
-        {
-            return false;
-        }
-
         public async Task<string> GenerateThumbnailBase64FromImageAsync(Stream imageStream)
         {
-            return await Task.Factory.StartNew(() =>
+            using (var image = await SixLabors.ImageSharp.Image.LoadAsync(imageStream))
             {
-                Image.GetThumbnailImageAbort myCallback = new Image.GetThumbnailImageAbort(ThumbnailCallback);
-                Bitmap bmp = new Bitmap(imageStream);
-
-                int width = 600;
-                int X = bmp.Width;
-                int Y = bmp.Height;
-                int height = (width * Y) / X;
-
-                var thumbnail = bmp.GetThumbnailImage(width, height, myCallback, IntPtr.Zero);
-                MemoryStream thumbStream = new MemoryStream();
-                thumbnail.Save(thumbStream, System.Drawing.Imaging.ImageFormat.Jpeg);
-                thumbStream.Position = 0;
-                return Convert.ToBase64String(thumbStream.ToArray());
-            });
+                image.Mutate(x => x.Resize(600, 0, KnownResamplers.Lanczos3));
+                using (MemoryStream mstream = new MemoryStream())
+                {
+                    await image.SaveAsync(mstream, new JpegEncoder());
+                    return Convert.ToBase64String(mstream.ToArray());
+                }
+            }
         }
     }
 }
